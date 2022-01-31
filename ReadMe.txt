a79d97ddd0b94e32bd39c72eb48e52b295cff1da
ReboOnline <70451563+ReboOnline@users.noreply.github.com>
2022-01-31 06:38:30 +0100

    new site 1
    This is the first upload of the new green website

--- ReadMe.txt
+++
@@ -1,6 +0,0 @@
-This site is only for free notes.
-
-You may use it as you want on you own risk
-
-last change1
-dit is the one and only Website op Github!!!
---
+++ js/index.js
@@ -0,0 +1,24 @@
+<div class="css-script-ads"><script type="text/javascript">
+google_ad_client = "ca-pub-2783044520727903";
+/* CSSScript Demo Page */
+google_ad_slot = "3025259193";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script> 
+      <scrip type="text/javascript"
+src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></div>
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46156385-1', 'cssscript.com');
+  ga('send', 'pageview');
+
+
+
+
